c6013ff807b60074fb5753f7655ff9d15c521d39
lennartnat <lennartnat@live.com>
2019-09-09 23:37:01 +0800

    Fixed typo in homepage

--- pages/index.js
+++ pages/index.js
@@ -14,7 +14,7 @@ const Home = () => {
       <div className={style.etc}>
         <p>I've created this project to serve as my Next.js playground.</p>
         <p>I'm still thinking about what to build.</p>
-        <p>For the meantime, hake a look at the following:</p>
+        <p>For the meantime, have a look at the following:</p>
         <br />
         <Button link={'https://github.com/lennartnat'}>Github</Button>
         <Button link={'https://www.linkedin.com/in/aldrine-lennart-yap/'}>LinkedIn</Button>
